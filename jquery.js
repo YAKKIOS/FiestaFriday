c39406f4a98f3eed45fbff08a22d9c326169bc60
Jack Johnson <jackjohnosn036@gmail.com>
2019-09-24 16:06:28 +0100

    Further updates

--- jquery.js
+++ jquery.js
@@ -1,16 +1,10 @@
- if( bottom_of_window > bottom_of_object ){
-            $(this).addClass('.background.png');
-       }
-        if( bottom_of_window < bottom_of_object ){
-            $(this).removeClass('.background');
-
 $(document).ready(function() {
     
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
     
         /* Check the location of each desired element */
-        $('.background').each( function(i){
+        $('.quote').each( function(i){
             
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -18,7 +12,7 @@ $(document).ready(function() {
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
                 
-                $(this).animate({'opacity':'0'},500);
+                $(this).animate({'opacity':'1'},500);
                     
             }
             
