cc79df8239eaefe7bcfc819adb6ac1870caf0156
Jack Johnson <jackjohnosn036@gmail.com>
2019-09-26 15:04:36 +0100

    Added shirt section and scrolling text

--- jquery.js
+++ jquery.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $(window).scroll( function(){
     
         /* Check the location of each desired element */
-        $('.quote').each( function(i){
+        $('.content-playlist, .content-shirt').each( function(i){
             
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
